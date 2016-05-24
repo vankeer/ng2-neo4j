09e0933ea64e9e516b896560c38393d62253cb31
Laurens Van Keer <laurens.vankeer@betssongroup.com>
2016-05-24 14:52:54 +0200

    fix(Neo4jService): service should not implement OnInit

--- src/neo4j.service.ts
+++ src/neo4j.service.ts
@@ -1,30 +1,26 @@
-import {Injectable, Inject, OnInit} from '@angular/core';
+import {Injectable, Inject} from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {Observable} from "rxjs";
 import {Neo4jSettings} from "./neo4j-settings.model";
 
 @Injectable()
-export class Neo4jService implements OnInit {
-
+export class Neo4jService {
     private defaultOptions: RequestOptions;
 
     constructor(
         private http: Http,
         @Inject('Neo4jSettings') private settings: Neo4jSettings
     ) {
+		let headers = new Headers();
+		headers.append('Authorization', 'Basic ' + btoa(`${this.settings.username}:${this.settings.password}`));
+		headers.append('Content-Type', 'application/json');
+		this.defaultOptions = new RequestOptions({
+			headers: headers
+		});
     }
 
-    // todo: return type
-    getRoot(): any {
-        return this.http.get(`${this.settings.endpoint}/db/data/`, this.defaultOptions).map(res => res.json());
-    }
-
-    ngOnInit() {
-        let headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(`${this.settings.username}:${this.settings.password}`));
-        headers.append('Content-Type', 'application/json');
-        this.defaultOptions = new RequestOptions({
-            headers: headers
-        });
-    }
+	getNodesByLabel(label: string): Observable<any[]> {
+		return this.http.get(`${this.settings.endpoint}/db/data/label/${label}/nodes`, this.defaultOptions)
+			.map(res => res.json());
+	}
 }
